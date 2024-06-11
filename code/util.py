bfce12b5c548764c8fbbef554f0252d1bf87489d
Jonne Sälevä <jonne@jonnesaleva.com>
2024-06-11 00:42:48 -0400

    .

---
+++ code/util.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+from collections import namedtuple
+from pathlib import Path
+
+import click
+import duckdb
+import spacy
+from loguru import logger
+
+DEFAULT_DB_NAME = "data/corpus.duckdb"
+
+
+def output_as_jsonl(result):
+    stdout = click.get_text_stream("stdout")
+    result.to_json(stdout, orient="records", lines=True)
+
+
+def output_as_tsv(result):
+    stdout = click.get_text_stream("stdout")
+    result.to_csv(stdout, sep="\t", index=False)
+
+
+def output_based_on_format(result, output_format):
+    output_fn = {
+        "jsonl": output_as_jsonl,
+        "tsv": output_as_tsv,
+    }.get(output_format, click.echo)
+
+    output_fn(result)
+
+
+def safely_grab_db(db):
+    if not db:
+        import os
+
+        db = (
+            os.environ["DUCKDB_DATABASE"]
+
+            if "DUCKDB_DATABASE" in os.environ
+            else DEFAULT_DB_NAME
+        )
+
+    return db
